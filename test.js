b79536355fc60475b00ff7d025e3b455dea1bb9b
sadilapunchihewa <sadilapunchihewa03@gmail.com>
2026-09-21 13:00:06 +0530

    feat: add calculator and CI pipeline

---
+++ test.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(2, 3);
+
+if (result === 5) {
+  console.log("Test passed!");
+} else {
+  console.log("Test failed!");
+  process.exit(1);
+}
